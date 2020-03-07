2d102818d669acfaa7240fd9bffb6ba53d5ce21a
isaac <isaac.ingles@gmail.com>
2020-03-07 14:50:27 -0700

    added prisma/resolvers in index.js

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 // Using NPM's esm module so we can use ES6 syntax
 
 import { prisma } from './generated/prisma-client'
+import { graphQLServer } from 'graphql-yoga'
 
-// A 'main' function so that we can use async/await
-async function main() {
-  // Read the previously created user from the database and print their posts to the console
-  const postsByUser = await prisma.user({ email: 'bob@prisma.io' }).posts()
-  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
+const resolovers = {
+    Query: {
+        publishedPosts(root, args, context) {
+            return context.prisma.posts({ where: { published: true }})
+        },
+        post(root, args, context) {
+            return context.prisma.post({ id: args.postId })
+        },
+        postsByUser(root, args, context) {
+            return context.prisma
+            .user({
+                id: args.userId
+            })
+            .posts()
+        },
+    },
+
+    Mutation: {
+        createDraft(root, args, context) {
+            return context.prisma.createPost({
+                title: args.title,
+                author: {
+                    connect: { id: args.userId }
+                }
+            })
+        },
+        publish(root, args, context) {
+            return context.prisma.updatePost({
+                where: { id: args.postId },
+                data: { published: true }
+            })
+        },
+        createUser(root, args, context) {
+            return context.prisma.createUser({ name: args.name })
+        }
+    },
+    User: {
+        posts(root, args, context) {
+            return context.prisma
+            .user({
+                id: root.id
+            })
+            .posts()
+        }
+    },
+    Post: {
+        author(root, args, context) {
+            return context.prisma
+            .post({
+                id: root.id
+            })
+            .author()
+        }
+    }
 }
-
-main().catch(e => console.error(e))
